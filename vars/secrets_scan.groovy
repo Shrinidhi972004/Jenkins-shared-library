d0f9a33c9f76e1e3ad7d32d35fd894c8a119f5d5
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-09-28 14:35:37 +0530

    Set GitPython executable path in secrets scan
    Updated secrets_scan.groovy to set GIT_PYTHON_GIT_EXECUTABLE environment variable before running trufflehog.

--- vars/secrets_scan.groovy
+++ vars/secrets_scan.groovy
@@ -1,8 +1,10 @@
 def call(Map config = [:]) {
-    stage(config.stageName ?: 'Secrets Scan') {
-        def path = config.path ?: "."
+    stage(config.stageName ?: 'Secrets Scan') {   // Stage name defaults to "Secrets Scan"
+        def path = config.path ?: "."            // Path defaults to current dir
+
         sh """
-          ${env.TRUFFLEHOG_PATH} filesystem ${path} || true
+          export GIT_PYTHON_GIT_EXECUTABLE=/usr/bin/git   # Tell GitPython where git is
+          /var/lib/jenkins/venvs/trufflehog/bin/trufflehog filesystem ${path} || true
         """
     }
 }
