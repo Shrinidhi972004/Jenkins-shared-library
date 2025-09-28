245eecec0a111d92058759b5884828884881f248
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-09-28 16:29:11 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,9 +1,5 @@
 def call(Map config = [:]) {
-    stage(config.stageName ?: 'Docker Build') {
-        def image = config.image ?: "my-image"
-        def tag = config.tag ?: "latest"
-        def context = config.context ?: "."
-
-        sh "docker build -t ${image}:${tag} ${context}"
+    stage("Docker Build") {
+        sh "docker build -t ${config.image}:${config.tag} ."
     }
 }
