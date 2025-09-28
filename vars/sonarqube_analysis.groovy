a7ece46bcd61b6d1416b6d0d6b0df0b90022dc74
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-09-28 14:37:44 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,11 +1,11 @@
 def call(Map config = [:]) {
     stage(config.stageName ?: 'SonarQube Analysis') {
-        def scannerHome = tool 'SonarScanner'   // name from Jenkins Tool config
+        def scannerHome = tool 'SonarScanner'   // From Jenkins Global Tool Config
         withSonarQubeEnv(config.server ?: 'Sonar') {
             sh """
               ${scannerHome}/bin/sonar-scanner \
                 -Dsonar.projectKey=${config.projectKey ?: 'default-project'} \
-                -Dsonar.projectName=${config.projectName ?: 'default'} \
+                -Dsonar.projectName='${config.projectName ?: "default"}' \
                 -Dsonar.sources=${config.sources ?: '.'}
             """
         }
