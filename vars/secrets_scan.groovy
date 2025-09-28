c0d1ccfd5a8f76e066a38ac0fddd7a2dadf9155a
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-09-28 16:11:10 +0530

    Change secrets scan to use git instead of filesystem

--- vars/secrets_scan.groovy
+++ vars/secrets_scan.groovy
@@ -1,10 +1,9 @@
 def call(Map config = [:]) {
-    stage(config.stageName ?: 'Secrets Scan') {   // Stage name defaults to "Secrets Scan"
-        def path = config.path ?: "."            // Path defaults to current dir
-
+    stage("Secrets Scan") {
+        def path = config.path ?: "."
         sh """
-          export GIT_PYTHON_GIT_EXECUTABLE=/usr/bin/git   # Tell GitPython where git is
-          /var/lib/jenkins/venvs/trufflehog/bin/trufflehog filesystem ${path} || true
+          export GIT_PYTHON_GIT_EXECUTABLE=/usr/bin/git
+          /var/lib/jenkins/venvs/trufflehog/bin/trufflehog git file://${path} || true
         """
     }
 }
