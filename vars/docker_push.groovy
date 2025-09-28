650e918d96a92a56eeb5bba9c806089f5386f26b
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-09-28 16:29:44 +0530

    Refactor docker_push to use config parameters directly

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,17 +1,11 @@
 def call(Map config = [:]) {
-    stage(config.stageName ?: 'Docker Push') {
-        def image = config.image ?: "my-image"
-        def tag = config.tag ?: "latest"
-        def creds = config.credentialsId ?: "docker-credentials"
-
-        withCredentials([usernamePassword(
-            credentialsId: creds,
-            usernameVariable: 'DOCKER_USER',
-            passwordVariable: 'DOCKER_PASS'
-        )]) {
+    stage("Docker Push") {
+        withCredentials([usernamePassword(credentialsId: config.credentialsId,
+                                          usernameVariable: 'DOCKER_USER',
+                                          passwordVariable: 'DOCKER_PASS')]) {
             sh """
-              echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-              docker push ${image}:${tag}
+              echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+              docker push ${config.image}:${config.tag}
             """
         }
     }
